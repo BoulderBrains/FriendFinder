2729118db27616fda1542ef252f45a43e25c7295
Joshua <joshuadstarkey@gmail.com>
2019-03-14 13:46:32 -0700

    filling out friends survey results

--- app/data/friends.js
+++ app/data/friends.js
@@ -2,27 +2,27 @@ let friends = [
 	{
 		"name": "Jerry",
 		"photo": "https://bit.ly/2Fbjca3",
-		"scores": []
+		"scores": [3,3,3,1,1,5,1,5,5,1]
 	},
 	{
 		"name": "George",
 		"photo": "https://bit.ly/2T68Trw",
-		"scores": []
+		"scores": [5,3,3,3,4,1,4,3,5,4]
 	},
 	{
 		"name": "Elaine",
 		"photo": "https://bit.ly/2UC3VEc",
-		"scores": []
+		"scores": [1,3,3,2,4,5,3,4,1,4]
 	},
 	{
 		"name": "Kramer",
 		"photo": "https://bit.ly/2F4DEYE",
-		"scores": []
+		"scores": [3,1,1,5,4,5,1,2,4,3]
 	},
 	{
 		"name": "Newman",
 		"photo": "https://bit.ly/2Hi78Gh",
-		"scores": []
+		"scores": [3,3,2,5,4,5,5,1,5,1]
 	},
 ]
 
