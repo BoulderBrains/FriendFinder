2206e2aee724a0e0b57df73cedf4a92d7bb46a0a
Joshua <joshuadstarkey@gmail.com>
2019-03-14 10:43:48 -0700

    building out friend array

---
+++ app/data/friends.js
@@ -0,0 +1,29 @@
+let friends = [
+	{
+		"name": "Jerry",
+		"photo": "https://bit.ly/2Fbjca3",
+		"scores": []
+	},
+	{
+		"name": "George",
+		"photo": "https://bit.ly/2T68Trw",
+		"scores": []
+	},
+	{
+		"name": "Elaine",
+		"photo": "https://bit.ly/2UC3VEc",
+		"scores": []
+	},
+	{
+		"name": "Kramer",
+		"photo": "https://bit.ly/2F4DEYE",
+		"scores": []
+	},
+	{
+		"name": "Newman",
+		"photo": "https://bit.ly/2Hi78Gh",
+		"scores": []
+	},
+]
+
+module.exports = friends;
