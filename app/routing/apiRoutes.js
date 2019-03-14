fd329eb882cc4b55424756278350564a0d7f0570
Joshua <joshuadstarkey@gmail.com>
2019-03-14 14:09:59 -0700

    building out compatibilty function

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -7,6 +7,35 @@ module.exports = function(app) {
 
 	app.post("/api/friends", function(req, res) {
 		// console.log(req.body);
+		res.json(compatibility(req.body));
 		friendsData.push(req.body);
-	})
+	});
+
+	function compatibility(user) {
+		let surveyScoreTotal = function(obj) {
+			obj.scores.forEach(function(score) {
+				total += parseInt(socre);
+			});
+			return total;
+		}
+
+		let bestFriend = friendsData[0];
+		let friendScore = surveyScoreTotal(user);
+		let userScore = surveyScoreTotal(user);
+		let difference = Math.abs(friendScore - userScore);
+
+		for (var i = 1; i < friendsData.length; i++) {
+			difference = difference;
+			friendScore = surveyScoreTotal(friendsData[i]);
+			difference = Math.abs(friendScore - userScore);
+
+			if (difference > surveyScoreTotal) {
+				bestFriend = friendsData[i];
+			} else {
+				difference = difference;
+			}
+		}
+
+		return bestFriend;
+	}
 }
